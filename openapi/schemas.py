ded7ea43f998792899dbf0c6103b7150e3c47f2e
Will Smith <wsmith@linode.com>
2018-05-18 13:06:08 -0400

    Added dynamic Model creation
    This is a step toward calling operations generating Models for the
    responses - Schemas can now create Models which will represent them, and
    Models have some base logic to make them useful.  All Models from a
    single Schema will have the same Type.

--- openapi/schemas.py
+++ openapi/schemas.py
@@ -2,6 +2,9 @@ from .object_base import ObjectBase
 
 class Schema(ObjectBase):
     """
+    The `Schema Object`_ allows the definition of input and output data types.
+
+    .. _Schema Object: https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.1.md#schemaObject
     """
     __slots__ = ['title','multipleOf','maximum','exclusiveMaximum','minimum',
                  'exclusiveMinimum','maxLength','minLength','pattern','maxItems',
@@ -9,7 +12,7 @@ class Schema(ObjectBase):
                  'required','enum','type','allOf','oneOf','anyOf','not','items',
                  'properties','additionalProperties','description','format',
                  'default','nullable','discriminator','readOnly','writeOnly',
-                 'xml','externalDocs','example','deprecated']
+                 'xml','externalDocs','example','deprecated','_model_type']
     required_fields = []
 
     def _parse_data(self):
@@ -51,3 +54,70 @@ class Schema(ObjectBase):
         self.externalDocs = self._get('externalDocs', dict)# 'ExternalDocs')
         #self.example = self._get('example', any?)
         self.deprecated = self._get('deprecated', bool)
+
+
+    def get_type(self):
+        """
+        Returns the Type that this schema represents.  This Type is created once
+        per Schema and cached so that all instances of the same schema are the
+        same Type.  For example::
+
+           object1 = example_schema.model({"some":"json"})
+           object2 = example_schema.model({"other":"json"})
+
+           isinstance(object1, example._schema.get_type()) # true
+           type(object1) == type(object2) # true
+        """
+        if self._model_type is None:
+            self._model_type = type(self.path[-1], (Model,), {
+                '__slots__': self.properties.keys()
+            })
+
+        return self._model_type
+
+    def model(self, data):
+        """
+        Generates a model representing this schema from the given data.
+
+        :param data: The data to create the model from.  Should match this schema.
+        :type data: dict
+
+        :returns: A new :any:`Model` created in this Schema's type from the data.
+        :rtype: self.get_type()
+        """
+        return self.get_type()(data)
+
+
+class Model:
+    """
+    A Model is a representation of a Schema as a request or response.  Models
+    are generated from Schema objects by called :any:`Schema.model` with the
+    contents of a response.
+    """
+    __slots__ = ['_raw_data']
+    def __init__(self, data):
+        """
+        Creates a new Model from data.  This should never be called directly,
+        but instead should be called through :any:`Schema.model` to generate a
+        Model from a defined Schema.
+
+        :param data: The data to create this Model with
+        :type data: dict
+        """
+        self._raw_data = data
+
+        # collect the data into this model
+        for k, v in data.items():
+            setattr(self, k, v)
+
+    def __repr__(self):
+        """
+        A generic representation of this model
+        """
+        return str(self.__dict__())
+
+    def __dict__(self):
+        """
+        This object as a dict
+        """
+        return {s: getattr(self, s) for s in self.__slots__ if not s.startswith('_')}
