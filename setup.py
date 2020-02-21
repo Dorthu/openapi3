8a3a0b940ea5b5cb7997b69a92ab2caf2d47ea56
Sebastian Jordan <s.jordan@s-v.de>
2020-02-21 14:35:24 +0100

    Configure setuptools in setup.cfg instead of setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
 #!/usr/bin/env python3
-from io import open
 from setuptools import setup
-from os import path
-import subprocess
 
-
-here = path.abspath(path.dirname(__file__))
-
-
-# get the long description from the README.rst
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
-
-setup(
-    name="openapi3",
-    version='1.0.0',
-    description="Client and Validator of OpenAPI 3 Specifications",
-    long_description=long_description,
-    author="dorthu",
-    url="https://github.com/dorthu/openapi3",
-    packages=['openapi3'],
-    license="BSD 3-Clause License",
-    install_requires=["PyYaml", "requests"],
-)
+setup()
