17b3a1a2f0dd1f0eb38b78c941be357016f2a810
Markus Koetter <koetter@cispa.de>
2022-01-20 09:20:11 +0100

    model - extract functions

--- aiopenapi3/model.py
+++ aiopenapi3/model.py
@@ -28,79 +28,6 @@ class Model(BaseModel):
         if discriminators is None:
             discriminators = []
 
-        def typeof(schema: "SchemaBase"):
-            r = None
-            if schema.type == "integer":
-                r = int
-            elif schema.type == "number":
-                r = float
-            elif schema.type == "string":
-                r = str
-            elif schema.type == "boolean":
-                r = bool
-            elif schema.type == "array":
-                r = List[schema.items.get_type()]
-            elif schema.type == "object":
-                return schema.get_type()
-            elif schema.type is None:  # discriminated root
-                return None
-            else:
-                raise TypeError(schema.type)
-
-            return r
-
-        def annotationsof(schema: "SchemaBase"):
-            annos = dict()
-            if schema.type == "array":
-                annos["__root__"] = typeof(schema)
-            else:
-
-                for name, f in schema.properties.items():
-                    r = None
-                    for discriminator in discriminators:
-                        if name != discriminator.propertyName:
-                            continue
-                        for disc, v in discriminator.mapping.items():
-                            if v in shmanm:
-                                r = Literal[disc]
-                                break
-                        else:
-                            raise ValueError(schema)
-                        break
-                    else:
-                        r = typeof(f)
-
-                    from . import v20, v30, v31
-
-                    if isinstance(schema, (v20.Schema, v20.Reference)):
-                        if not f.required:
-                            annos[name] = Optional[r]
-                        else:
-                            annos[name] = r
-                    elif isinstance(schema, (v30.Schema, v31.Schema, v30.Reference, v31.Reference)):
-                        if name not in schema.required:
-                            annos[name] = Optional[r]
-                        else:
-                            annos[name] = r
-                    else:
-                        raise TypeError(schema)
-
-            return annos
-
-        def fieldof(schema: "SchemaBase"):
-            r = dict()
-            if schema.type == "array":
-                return r
-            else:
-                for name, f in schema.properties.items():
-                    args = dict()
-                    for i in ["enum", "default"]:
-                        v = getattr(f, i, None)
-                        if v:
-                            args[i] = v
-                    r[name] = Field(**args)
-            return r
-
         # do not create models for primitive types
         if shma.type in ("string", "integer", "number", "boolean"):
             return Model.typeof(shma)
@@ -110,7 +37,7 @@ class Model(BaseModel):
         annos = dict()
         if shma.allOf:
             for i in shma.allOf:
-                annos.update(annotationsof(i))
+                annos.update(Model.annotationsof(i, discriminators, shmanm))
         elif hasattr(shma, "anyOf") and shma.anyOf:
             t = tuple(
                 i.get_type(names=shmanm + [i.ref], discriminators=discriminators + [shma.discriminator])
@@ -130,11 +57,87 @@ class Model(BaseModel):
             else:
                 annos["__root__"] = Union[t]
         else:
-            annos = annotationsof(shma)
-            namespace.update(fieldof(shma))
+            annos = Model.annotationsof(shma, discriminators, shmanm)
+            namespace.update(Model.fieldof(shma))
 
         namespace["__annotations__"] = annos
 
         m = types.new_class(type_name, (BaseModel,), {}, lambda ns: ns.update(namespace))
         m.update_forward_refs()
         return m
+
+    @staticmethod
+    def typeof(schema: "SchemaBase"):
+        r = None
+        if schema.type == "integer":
+            r = int
+        elif schema.type == "number":
+            r = float
+        elif schema.type == "string":
+            r = str
+        elif schema.type == "boolean":
+            r = bool
+        elif schema.type == "array":
+            r = List[schema.items.get_type()]
+        elif schema.type == "object":
+            return schema.get_type()
+        elif schema.type is None:  # discriminated root
+            return None
+        else:
+            raise TypeError(schema.type)
+
+        return r
+
+    @staticmethod
+    def annotationsof(schema: "SchemaBase", discriminators, shmanm):
+        annos = dict()
+        if schema.type == "array":
+            annos["__root__"] = Model.typeof(schema)
+        else:
+
+            for name, f in schema.properties.items():
+                r = None
+                for discriminator in discriminators:
+                    if name != discriminator.propertyName:
+                        continue
+                    for disc, v in discriminator.mapping.items():
+                        if v in shmanm:
+                            r = Literal[disc]
+                            break
+                    else:
+                        raise ValueError(schema)
+                    break
+                else:
+                    r = Model.typeof(f)
+
+                from . import v20, v30, v31
+
+                if isinstance(schema, (v20.Schema, v20.Reference)):
+                    if not f.required:
+                        annos[name] = Optional[r]
+                    else:
+                        annos[name] = r
+                elif isinstance(schema, (v30.Schema, v31.Schema, v30.Reference, v31.Reference)):
+                    if name not in schema.required:
+                        annos[name] = Optional[r]
+                    else:
+                        annos[name] = r
+                else:
+                    raise TypeError(schema)
+
+        return annos
+
+    @staticmethod
+    def fieldof(schema: "SchemaBase"):
+        r = dict()
+        if schema.type == "array":
+            return r
+        else:
+            for name, f in schema.properties.items():
+                args = dict()
+                for i in ["enum", "default"]:
+                    v = getattr(f, i, None)
+                    if v:
+                        args[i] = v
+                r[name] = Field(**args)
+        return r
